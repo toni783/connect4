0170007044981e2158af41987f55c029abf05cf3
Gilbert Morett <toni7858@gmail.com>
2022-11-24 14:10:22 -0500

    feat: added bootstrap styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
 
-import store from '../store'
+import store from "../store";
+/**bootstrap styles */
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  )
+  );
 }
