4781d785783d48b332ee33300f227807fd99f860
Gilbert Morett <toni7858@gmail.com>
2022-11-29 17:28:43 -0500

    feat: added axios custom query and initial rtk api setup

---
+++ src/utils/axios.ts
@@ -0,0 +1,40 @@
+import { createApi, BaseQueryFn } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+import type { AxiosRequestConfig, AxiosError } from "axios";
+
+const axiosBaseQuery =
+  (
+    { baseUrl }: { baseUrl: string } = { baseUrl: "" }
+  ): BaseQueryFn<
+    {
+      url: string;
+      method: AxiosRequestConfig["method"];
+      data?: AxiosRequestConfig["data"];
+      params?: AxiosRequestConfig["params"];
+    },
+    unknown,
+    unknown
+  > =>
+  async ({ url, method, data, params }) => {
+    try {
+      const result = await axios({ url: baseUrl + url, method, data, params });
+      return { data: result.data };
+    } catch (axiosError) {
+      let err = axiosError as AxiosError;
+      return {
+        error: {
+          status: err.response?.status,
+          data: err.response?.data || err.message,
+        },
+      };
+    }
+  };
+
+export const GAMES_TAGS = "Games";
+
+// initialize an empty api service that we'll inject endpoints into later as needed
+export const emptySplitApi = createApi({
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  endpoints: () => ({}),
+  tagTypes: [GAMES_TAGS],
+});
