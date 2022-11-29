882cb08b86662b9715fd22ccb98c0aa5876f012c
Gilbert Morett <toni7858@gmail.com>
2022-11-29 17:44:52 -0500

    feat: updated config to properly use boardApi

--- src/store.ts
+++ src/store.ts
@@ -4,6 +4,7 @@ import {
   Action,
   combineReducers,
 } from "@reduxjs/toolkit";
+import { boardApi } from "./features/board/BoardAPI";
 import boardReducer from "./features/board/boardSlice";
 
 import counterReducer from "./features/counter/counterSlice";
@@ -11,11 +12,14 @@ import counterReducer from "./features/counter/counterSlice";
 const reducer = combineReducers({
   counter: counterReducer,
   board: boardReducer,
+  [boardApi.reducerPath]: boardApi.reducer,
 });
 
 export function makeStore() {
   return configureStore({
     reducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(boardApi.middleware),
   });
 }
 
