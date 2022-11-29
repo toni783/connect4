6ee652fb611762574b89b32c6fdf82e24c822e8c
Gilbert Morett <toni7858@gmail.com>
2022-11-29 17:26:56 -0500

    feat: added database util for connection to database

---
+++ src/utils/database.ts
@@ -0,0 +1,15 @@
+import { Pool } from "pg";
+
+let conn: any;
+
+if (!conn) {
+  conn = new Pool({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_DATABASE,
+  });
+}
+
+export { conn };
