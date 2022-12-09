6fdaaf029d8e9ffbd9b944a8e0037883c5b0181b
Gilbert Morett <toni7858@gmail.com>
2022-12-08 22:10:04 -0500

    feat: updated jest config to properly use paths on tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,25 @@
-import type { InitialOptionsTsJest } from 'ts-jest/dist/types'
+import type { InitialOptionsTsJest } from "ts-jest/dist/types";
 
 const config: InitialOptionsTsJest = {
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   transform: {
-    '.+\\.(css|styl|less|sass|scss)$': 'jest-css-modules-transform',
+    "^.+\\.jsx?$": "babel-jest",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.test.json',
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
     },
   },
-}
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    axios: "axios/dist/node/axios.cjs",
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy", // added to mock styles files (https://jestjs.io/docs/en/webpack#mocking-css-modules)
+  },
+  transformIgnorePatterns: ["/node_modules/(?!axios).+\\.js$"],
+};
 
-export default config
+export default config;
