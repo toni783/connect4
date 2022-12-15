32bcd30eb1762d974927722d462d3c36030604ec
Gilbert Morett <toni7858@gmail.com>
2022-12-15 01:58:35 -0500

    feat: added custom type for optional types with required fields

--- src/types/common.ts
+++ src/types/common.ts
@@ -4,3 +4,9 @@
 export type WithRequiredProperty<Type, Key extends keyof Type> = Type & {
   [Property in Key]-?: Type[Property];
 };
+
+/**
+ * Type for making all fields optional except for the ones that we indicate
+ */
+export type OptionalExceptFor<T, TRequired extends keyof T> = Partial<T> &
+  Pick<T, TRequired>;
