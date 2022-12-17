b64c3d9257693ffe03de53405344af4138c12ab2
Gilbert Morett <toni7858@gmail.com>
2022-12-17 15:17:54 -0500

    chore: update util name to be more descriptive

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -4,6 +4,6 @@
  * @param max maximum number for random number between specified range
  * @returns a number between the min and max range
  */
-export const randomNumber = (min: number, max: number) => {
+export const randomNumberInRange = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
